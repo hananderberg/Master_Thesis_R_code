67bc1a3658813fc5e9610056a6cc26e466d7f4e8
Sofia <sofia.wadell@gmail.com>
2023-03-24 16:10:38 +0100

    Final code

--- MICE.R
+++ MICE.R
@@ -30,14 +30,17 @@ train_miss_data_x <- load_train_data(data_name,missingness_percentage)
 head(train_miss_data_x)
 str(train_miss_data_x)
 summary(train_miss_data_x)
+sapply(train_miss_data_x, function(x) sum(is.na(x)))
 
 # Load normalized test data
 test_miss_data_x <- load_test_data(data_name,missingness_percentage)
 
 # Control
 head(test_miss_data_x)
+
 str(test_miss_data_x)
 summary(test_miss_data_x)
+sapply(test_miss_data_x, function(x) sum(is.na(x)))
 
 ### Factor encode categorical variables 
 train_miss_data_x_encoded <- factor_encode(train_miss_data_x)
@@ -52,12 +55,15 @@ nbr_of_training_rows = nrow(train_miss_data_x_encoded)
 nbr_of_testing_rows = nrow(test_miss_data_x_encoded)
 full_miss_data_x = rbind(train_miss_data_x_encoded, test_miss_data_x_encoded)
 
-# Control
-str(full_miss_data_x)
-
 ####################  3. Impute using MICE  ####################
-imp <- mice(full_miss_data_x, ignore = c(rep(FALSE, nbr_of_training_rows), rep(TRUE, nbr_of_testing_rows)), defaultMethod = c("norm", 'logreg', 'polyreg', 'polr'), m = ncol(full_miss_data_x), seed = 1)
+timing <- system.time({
+  imp <- mice(full_miss_data_x, ignore = c(rep(FALSE, nbr_of_training_rows), rep(TRUE, nbr_of_testing_rows)), defaultMethod = c("norm", 'logreg', 'polyreg', 'polr'), m = ncol(full_miss_data_x), seed = 1)
+  })
+
+# Printing the execution time
+cat("Execution time:", timing[3], "seconds\n")
 completed_dataset <- complete(imp)
+
 
 # Test if NA's left  
 sapply(completed_dataset, function(x) sum(is.na(x)))
@@ -71,11 +77,11 @@ train_imp_data_x <- completed_dataset[1:nbr_of_training_rows, ]
 test_imp_data_x <- completed_dataset[(nbr_of_training_rows+1):nrow(completed_dataset), ]
 
 # Save train data
-file_path <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/imputed_train_data/imputed_", data_name, "_train_", missingness_percentage, ".csv")
+file_path <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/norm_imputed_mice_train_data/norm_imputed_mice_", data_name, "_train_", missingness_percentage, ".csv")
 write.csv(train_imp_data_x, file_path, row.names = FALSE)
 
 # Save test data
-file_path <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/imputed_test_data/imputed_", data_name, "_test_", missingness_percentage, ".csv")
+file_path <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/norm_imputed_mice_test_data/norm_imputed_mice_", data_name, "_test_", missingness_percentage, ".csv")
 write.csv(test_imp_data_x, file_path, row.names = FALSE)
 
 
--- help_functions_mice.R
+++ help_functions_mice.R
@@ -1,13 +1,13 @@
 # Load normalized train data without target column
 load_train_data <- function(data_name, missingness_percentage) {
-  file_name_miss_train <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/norm_factor_train_data_wo_target/norm_factor_encode_", data_name, "_train_",missingness_percentage,".csv")
+  file_name_miss_train <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/norm_factor_train_data_wo_target_for_mice/norm_factor_for_mice_", data_name, "_train_",missingness_percentage,".csv")
   train_miss_data_x <- read.csv(file_name_miss_train,header=TRUE, sep=",")
   return(train_miss_data_x)
 }
 
 # Load normalized test data without target column 
 load_test_data <- function(data_name, missingness_percentage) {
-  file_name_miss_test <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/norm_factor_test_data_wo_target/norm_factor_encode_", data_name, "_test_",missingness_percentage,".csv")
+  file_name_miss_test <- paste0("/Users/sofiawadell/Documents/MasterThesis/R_v2/Master_Thesis_R_code/norm_factor_test_data_wo_target_for_mice/norm_factor_for_mice_", data_name, "_test_",missingness_percentage,".csv")
   test_miss_data_x <- read.csv(file_name_miss_test,header=TRUE, sep=",")
   return(test_miss_data_x)
 }
